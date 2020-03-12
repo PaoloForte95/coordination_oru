fdf40b9143503d073363ea62f53c5e0a54530b95
PaoloForte95 <paolo.forte1995@gmail.com>
2020-03-12 14:27:29 +0100

    New Example with Robots in a grid

---
+++ src/main/java/se/oru/coordination/coordination_oru/taskassignment/test/TaskAssignmentRobotsGrid.java
@@ -0,0 +1,135 @@
+package se.oru.coordination.coordination_oru.taskassignment.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+import org.metacsp.multi.spatioTemporal.paths.Pose;
+import org.metacsp.multi.spatioTemporal.paths.PoseSteering;
+import org.metacsp.multi.spatioTemporal.paths.TrajectoryEnvelope;
+import org.metacsp.multi.spatioTemporal.paths.TrajectoryEnvelope.SpatialEnvelope;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Polygon;
+
+import aima.core.agent.Model;
+import se.oru.coordination.coordination_oru.ConstantAccelerationForwardModel;
+import se.oru.coordination.coordination_oru.CriticalSection;
+import se.oru.coordination.coordination_oru.Mission;
+import se.oru.coordination.coordination_oru.RobotAtCriticalSection;
+import se.oru.coordination.coordination_oru.RobotReport;
+import se.oru.coordination.coordination_oru.demo.DemoDescription;
+import se.oru.coordination.coordination_oru.motionplanning.ompl.ReedsSheppCarPlanner;
+import se.oru.coordination.coordination_oru.simulation2D.TimedTrajectoryEnvelopeCoordinatorSimulation;
+import se.oru.coordination.coordination_oru.simulation2D.TrajectoryEnvelopeCoordinatorSimulation;
+import se.oru.coordination.coordination_oru.util.BrowserVisualization;
+import se.oru.coordination.coordination_oru.util.JTSDrawingPanelVisualization;
+import se.oru.coordination.coordination_oru.util.Missions;
+
+
+
+
+import se.oru.coordination.coordination_oru.taskassignment.TaskAssignment;
+
+
+
+import org.metacsp.multi.spatioTemporal.paths.Pose;
+import org.metacsp.multi.spatioTemporal.paths.PoseSteering;
+import org.metacsp.multi.spatioTemporal.paths.Trajectory;
+import org.metacsp.multi.spatioTemporal.paths.TrajectoryEnvelope;
+
+import se.oru.coordination.coordination_oru.taskassignment.Robot;
+import se.oru.coordination.coordination_oru.taskassignment.Task;
+
+
+
+
+import com.google.ortools.linearsolver.*;
+import com.google.ortools.linearsolver.MPSolver.ResultStatus;
+import com.google.ortools.linearsolver.PartialVariableAssignment;
+import com.google.ortools.constraintsolver.Solver;
+import com.google.ortools.constraintsolver.Solver;
+import com.google.ortools.sat.*;
+
+@DemoDescription(desc = "One-shot navigation of 3 robots coordinating on paths obtained with the ReedsSheppCarPlanner.")
+public class TaskAssignmentRobotsGrid {
+	//load library used for optimization
+	 static {
+		    System.loadLibrary("jniortools");
+		  }
+	public static void main(String[] args) throws InterruptedException {
+		//Max Vel and Acc for the robots
+		double MAX_ACCEL = 1.0;
+		double MAX_VEL = 4.0;
+		//Instantiate a timed trajectory envelope coordinator.
+		//You still need to add one or more comparators to determine robot orderings thru critical sections (comparators are evaluated in the order in which they are added)
+		//final TrajectoryEnvelopeCoordinatorSimulation tec = new TrajectoryEnvelopeCoordinatorSimulation(MAX_VEL,MAX_ACCEL);
+		final TimedTrajectoryEnvelopeCoordinatorSimulation tec = new TimedTrajectoryEnvelopeCoordinatorSimulation(MAX_VEL,MAX_ACCEL);
+		tec.addComparator(new Comparator<RobotAtCriticalSection> () {
+			@Override
+			public int compare(RobotAtCriticalSection o1, RobotAtCriticalSection o2) {
+				CriticalSection cs = o1.getCriticalSection();
+				RobotReport robotReport1 = o1.getRobotReport();
+				RobotReport robotReport2 = o2.getRobotReport();
+				return ((cs.getTe1Start()-robotReport1.getPathIndex())-(cs.getTe2Start()-robotReport2.getPathIndex()));
+			}
+		});
+		
+		tec.addComparator(new Comparator<RobotAtCriticalSection> () {
+			@Override
+			public int compare(RobotAtCriticalSection o1, RobotAtCriticalSection o2) {
+				return (o2.getRobotReport().getRobotID()-o1.getRobotReport().getRobotID());
+			}
+		});
+		
+		//Need to instantiate the fleetmaster interface
+		tec.instantiateFleetMaster(0.1, false);
+		Coordinate footprint1 = new Coordinate(-1.0,0.5);
+		Coordinate footprint2 = new Coordinate(1.0,0.5);
+		Coordinate footprint3 = new Coordinate(1.0,-0.5);
+		Coordinate footprint4 = new Coordinate(-1.0,-0.5);
+		tec.setDefaultFootprint(footprint1, footprint2, footprint3, footprint4);
+		//Need to setup infrastructure that maintains the representation
+		tec.setupSolver(0, 100000000);
+		//JTSDrawingPanelVisualization viz = new JTSDrawingPanelVisualization();
+		//viz.setSize(1024, 768);
+		BrowserVisualization viz = new BrowserVisualization();
+		viz.setInitialTransform(20, 0, 0);
+		tec.setVisualization(viz);
+		tec.setUseInternalCriticalPoints(false);
+
+		
+		//Instantiate a simple motion planner (no map given here, otherwise provide yaml file)
+		ReedsSheppCarPlanner rsp = new ReedsSheppCarPlanner();
+		rsp.setRadius(0.2);
+		rsp.setFootprint(footprint1,footprint2,footprint3,footprint4);
+		rsp.setTurningRadius(4.0);
+		rsp.setDistanceBetweenPathPoints(0.5);
+		
+		
+		TaskAssignment assignmentProblem = new TaskAssignment();
+		double delta = 0;
+		for(int i = 1; i<= 8; i++) {
+			
+			Pose startPoseRobot = new Pose(4.0,(6.0 + delta),0.0);
+			Robot robot = new Robot(i, startPoseRobot, new ConstantAccelerationForwardModel(MAX_ACCEL, MAX_VEL, tec.getTemporalResolution(), tec.getControlPeriod(), tec.getTrackingPeriod()));
+			tec.addRobot(robot);
+			Pose startPoseGoal = new Pose(15.0,(6.0 + delta),0.0);
+			Pose goalPoseRobot = new Pose(30.0 ,(6.0 + delta) ,0.0);
+			Task task = new Task(startPoseGoal,goalPoseRobot,1);
+			assignmentProblem.addTask(task);
+			delta += 6.0;
+		}
+		
+	
+		//Solve the problem to find some feasible solution
+		double alpha = 1;
+		assignmentProblem.setminMaxVelandAccel(MAX_VEL, MAX_ACCEL);
+		assignmentProblem.instantiateFleetMaster(0.1, false);
+		assignmentProblem.setDefaultMotionPlanner(rsp);
+		tec.setDefaultMotionPlanner(assignmentProblem.getDefaultMotionPlanner());
+		
+		assignmentProblem.startTaskAssignment(alpha, tec);
+	}
+}
