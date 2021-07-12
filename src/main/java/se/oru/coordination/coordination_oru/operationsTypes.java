fc473e7ee7fe4a88ef39fd5ab6b3a8f808824e98
PaoloForte95 <paolo.forte1995@gmail.com>
2021-07-12 16:28:48 +0200

    Add new operation types

--- src/main/java/se/oru/coordination/coordination_oru/operationsTypes.java
+++ src/main/java/se/oru/coordination/coordination_oru/operationsTypes.java
@@ -4,7 +4,7 @@ package se.oru.coordination.coordination_oru;
 public abstract class operationsTypes {
 	
 	
-	public static enum OPERATION_TYPE {NO_OPERATION, NAVIGATION, UNLOAD, LOAD};
+	public static enum OPERATION_TYPE {NO_OPERATION, NAVIGATION, UNLOAD, LOAD,APPROACH_MATERIAL,LOAD_DETECT};
 	
 }
 
